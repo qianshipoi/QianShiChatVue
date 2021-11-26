5e7adacb92ed988a714732b5bd48daed8e7b55af
qianshi <2410128587@qq.com>
2021-11-26 16:54:01 +0800

    添加环境变量

--- src/request/index.js
+++ src/request/index.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import router from "../router"
 
 const instance = axios.create({
-  baseURL: `${process.env.API_BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 })
 
 // 请求拦截器
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue()
   ],
-  define: {
-    'process.env': {
-      API_BASE_URL: "http://localhost:5000"
-    }
-  }
 })
